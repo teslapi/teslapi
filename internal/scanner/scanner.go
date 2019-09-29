36f39fac8b62bcf777fb7447878ca8b8fa6a82e1
Jason McCallister <themccallister@gmail.com>
2019-09-28 22:25:41 -0400

    cleaning up

--- internal/scanner/scanner.go
+++ internal/scanner/scanner.go
@@ -1,13 +1,16 @@
 package scanner
 
 import (
+	"crypto/md5"
+	"encoding/hex"
 	"os"
 	"path/filepath"
 )
 
 // Recording represents a recording
 type Recording struct {
-	Name string
+	ID   string `json:"id"`
+	Name string `json:"name"`
 }
 
 // Scan a directory and return all of the files
@@ -19,8 +22,14 @@ func Scan(path string) []Recording {
 			return err
 		}
 
+		hash := md5.Sum([]byte(info.Name()))
+		id := hex.EncodeToString(hash[:])
+
 		if info.IsDir() == false && info.Name() != ".DS_Store" {
-			files = append(files, Recording{Name: info.Name()})
+			files = append(files, Recording{
+				ID:   id,
+				Name: info.Name(),
+			})
 		}
 
 		return nil
