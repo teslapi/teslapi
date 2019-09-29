471ffd76642bec13a0fe8faac75b0b37d5a6133e
Jason McCallister <themccallister@gmail.com>
2019-09-29 12:34:47 -0400

    adds camera to scanner

--- internal/scanner/scanner.go
+++ internal/scanner/scanner.go
@@ -6,6 +6,8 @@ import (
 	"os"
 	"path/filepath"
 	"time"
+
+	"github.com/teslapi/teslapi/internal/parse"
 )
 
 // Recording represents a recording
@@ -13,6 +15,7 @@ type Recording struct {
 	ID        string    `json:"id"`
 	Name      string    `json:"name"`
 	Timestamp time.Time `json:"timestamp"`
+	Camera    string    `json:"camera"`
 }
 
 // Scan a directory and return all of the files
@@ -24,14 +27,19 @@ func Scan(path string) []Recording {
 			return err
 		}
 
+		// hash the filename for an ID
 		hash := md5.Sum([]byte(info.Name()))
 		id := hex.EncodeToString(hash[:])
+
+		// determine which camera this is for
+		camera := parse.CameraFromRecording(info.Name())
 
 		if info.IsDir() == false && info.Name() != ".DS_Store" {
 			files = append(files, Recording{
 				ID:        id,
 				Name:      info.Name(),
 				Timestamp: info.ModTime(),
+				Camera:    camera,
 			})
 		}
 
