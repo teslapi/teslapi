2bcbdf97ff8b86a5e570536dacc6915181adb097
Jason McCallister <themccallister@gmail.com>
2019-10-05 23:46:55 -0400

    wip

--- cmd/teslapid/teslapid.go
+++ cmd/teslapid/teslapid.go
@@ -1,44 +1,64 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
 	"os"
-
-	"github.com/dgraph-io/badger"
-	"github.com/teslapi/teslapi/internal/handlers"
+	"path/filepath"
+	"time"
 )
 
-var requiredEnvs = []string{
-	"TESLAPI_KEY",
-	"TESLAPI_USERNAME",
-	"TESLAPI_PASSWORD",
+type ClipsPageData struct {
+	Title string
+	Clips []clip
 }
 
-func init() {
-	// check the environment variables that are required for the app
-	for _, e := range requiredEnvs {
-		if env := os.Getenv(e); env == "" {
-			log.Println(env)
-			log.Fatalf("expected the environment variable %v to be defined", e)
-		}
-	}
+type clip struct {
+	Name           string    `json:"name"`            // e.g. 2019-03-04_17-20-front.mp4
+	Type           string    `json:"type"`            // could be recent or saved
+	Camera         string    `json:"camera"`          // e.g. front, right, left
+	FileLocation   string    `json:"file_location"`   // the root path of the file on disk, even if it is remote
+	RemoteLocation string    `json:"remote_location"` // the URL or path to the file on the cloud storage provider
+	Uploaded       bool      `json:"uploaded"`        // if the clip has been uploaded or not
+	FileTimestamp  time.Time `json:"file_timestamp"`  // timestamp of the file
+	DateUploaded   time.Time `json:"date_uploaded"`   // the date and time that the clip was uploaded to the cloud storage provider
 }
 
 func main() {
-	// scan the directory for files
+	// scan the directory for clips
+	files := []os.FileInfo{}
+	err := filepath.Walk("./storage/TeslaUSB", func(root string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
 
-	// open the database to store clips in
-	db, err := badger.Open(badger.DefaultOptions("../../storage/db"))
+		if info.IsDir() == false && info.Name() != ".DS_Store" {
+			files = append(files, info)
+		}
+
+		return nil
+	})
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer db.Close()
 
-	http.HandleFunc("/", handlers.UI(db))
-	http.HandleFunc("/login", handlers.Login())
-	http.HandleFunc("/api/login", handlers.Login())
-	http.HandleFunc("/api/recordings", handlers.Recordings())
+	// convert the files into clips
+	clips := []clip{}
+	for _, f := range files {
+		clips = append(clips, clip{
+			Name: f.Name(),
+		})
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		data := ClipsPageData{
+			Title: "Clips",
+			Clips: clips,
+		}
+		tmpl := template.Must(template.ParseFiles("templates/index.html"))
+		tmpl.Execute(w, data)
+	})
 
 	if err := http.ListenAndServe(":3000", nil); err != nil {
 		log.Fatalf("%v", err)
