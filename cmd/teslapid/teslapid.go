fdf38a1237d416bcce936a6cd16fd573ea576ca9
Jason McCallister <themccallister@gmail.com>
2019-10-10 19:25:37 -0400

    add handler for api/login

--- cmd/teslapid/teslapid.go
+++ cmd/teslapid/teslapid.go
@@ -8,8 +8,10 @@ import (
 	"os"
 	"path/filepath"
 	"regexp"
+	"time"
 
 	badger "github.com/dgraph-io/badger"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/teslapi/teslapi/internal/recordings"
 )
 
@@ -51,6 +53,61 @@ func main() {
 			Title: "Clips",
 			Clips: clips,
 		})
+	})
+
+	http.HandleFunc("api/login", func(w http.ResponseWriter, r *http.Request) {
+		type loginRequest struct {
+			Username string `json:"username"`
+			Password string `json:"password"`
+		}
+
+		type loginResponse struct {
+			Token string `json:"token"`
+		}
+
+		if r.Method != http.MethodPost {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			return
+		}
+
+		r.ParseForm()
+
+		request := loginRequest{}
+		err := json.NewDecoder(r.Body).Decode(&request)
+		if err != nil {
+			log.Fatal(err.Error())
+			return
+		}
+
+		if request.Username != os.Getenv("TESLAPI_USERNAME") || request.Password != os.Getenv("TESLAPI_PASSWORD") {
+			w.WriteHeader(http.StatusBadRequest)
+			log.Println("invalid request")
+			return
+		}
+
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
+			ExpiresAt: time.Now().Local().Add(time.Minute * 30).Unix(),
+			IssuedAt:  time.Now().Local().Unix(),
+			Issuer:    "teslapi",
+		})
+
+		// Sign and get the complete encoded token as a string using the secret
+		tokenString, err := token.SignedString([]byte(os.Getenv("TESLAPI_KEY")))
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+
+		response := loginResponse{Token: tokenString}
+		body, err := json.Marshal(&response)
+		if err != nil {
+			log.Fatal(err.Error())
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+		w.Write(body)
 	})
 
 	http.HandleFunc("/api/recordings", func(w http.ResponseWriter, r *http.Request) {
